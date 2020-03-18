9674397c08df7562a4b56166b7b47830710d898a
Ralph Lawrence Silaya <rrsilaya@gmail.com>
2020-03-18 20:53:42 +0800

    Initial commit :fire:

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    mode: 'development',
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './src/config' },
+            { from: './src/static' },
+        ])
+    ],
+};
+
+module.exports = config;
