f213ed435096291158020a72855ef6c3b070adf3
Ralph Lawrence Silaya <rrsilaya@gmail.com>
2020-04-25 09:32:52 +0800

    Allow support for facebook redesign

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,9 @@
 export const NodeSelector = {
     CONTEXT: 'span[class=_4a6n]',
     POST: '._3ccb',
+    FB_NEXT: 'body._6s5d',
+    CONTEXT_NEXT: '.k4urcfbm.kr520xx4.j9ispegn.pmk7jnqg.taijpn5t.datstx6m.cbu4d94t.j83agx80.bp9cbjyn .kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.c1et5uql',
+    POST_NEXT: '.sjgh65i0.l9j0dhe7.k4urcfbm.du4w35lb',
     SPOTIFY_TRACK_PARSED: 'a[href*="open.spotify.com/track/"',
     SPOTIFY_TRACK_UNPARSED: 'a[href*="https%3A%2F%2Fopen.spotify.com%2Ftrack%2F"',
 
